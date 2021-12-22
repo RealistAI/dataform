1ced05d75de3e756a86182b88d2eae6b229ec45d
Mackenzie Gilbert <mackenzie.gilbert@realist.ai>
2021-12-22 21:54:04 +0000

    attempting to make code able to workin cloud func

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const { exec } = require("child_process");
-
-function exec("sh run.sh", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
-
-
+const { exec } = require('child_process');
+var yourscript = exec('sh run.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
