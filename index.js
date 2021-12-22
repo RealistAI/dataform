5e715cbf29f49374dad4970a990c3b37e8467cf5
Mackenzie Gilbert <mackenzie.gilbert@realist.ai>
2021-12-22 21:51:34 +0000

    attempting to make code able to workin cloud func

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-function exec("dataform run", (error, stdout, stderr) => {
+function exec("sh run.sh", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
---
+++ run.sh
@@ -0,0 +1,1 @@
+echo "dataform run"
