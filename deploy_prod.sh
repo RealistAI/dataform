2cc3218334cdea9c74ff21e850ffe0d6de3da53a
MGilbert777 <mackenzie.gilbert@realist.ai>
2022-05-18 16:48:48 +0000

    Fixxed the problem that broke the master schedule calc.

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cat dataform_prod.json > dataform.json
+mv replace/whitestone_master_schedule_prod.sqlx definitions/reporting/ && mv definitions/reporting/whitestone_master_schedule_test.sqlx replace/
 dataform run
--- deploy_test.sh
+++ deploy_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cat dataform_test.json > dataform.json
+mv replace/whitestone_master_schedule_test.sqlx definitions/reporting/ && mv definitions/reporting/whitestone_master_schedule_prod.sqlx replace/
 dataform run
