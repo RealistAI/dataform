e47109f435df4b8fe41c04c10acbd11aa0f2caa4
MGilbert777 <mackenzie.gilbert@realist.ai>
2022-07-14 17:29:33 +0000

    Adding last features

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cat dataform_prod.json > dataform.json
-mv replace/whitestone_master_schedule_staging_prod.sqlx definitions/staging/ && mv definitions/staging/whitestone_master_schedule_staging_test.sqlx replace/
-mv replace/whitestone_master_schedule_prod.sqlx definitions/reporting/ && mv definitions/reporting/whitestone_master_schedule_test.sqlx replace/
+mv replace/whitestone_master_schedule_staging_prod.sqlx definitions/staging/
+mv definitions/staging/whitestone_master_schedule_staging_test.sqlx replace/
+mv replace/whitestone_master_schedule_prod.sqlx definitions/reporting/
+mv definitions/reporting/whitestone_master_schedule_test.sqlx replace/
+mv replace/whitestone_private_equity_schedule_prod.sqlx definitions/reporting/
+mv definitions/reporting/whitestone_private_equity_schedule_test.sqlx replace/
 dataform run
--- deploy_test.sh
+++ deploy_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cat dataform_test.json > dataform.json
-mv replace/whitestone_master_schedule_staging_test.sqlx definitions/staging/ && mv definitions/staging/whitestone_master_schedule_staging_prod.sqlx replace/
-mv replace/whitestone_master_schedule_test.sqlx definitions/reporting/ && mv definitions/reporting/whitestone_master_schedule_prod.sqlx replace/
+sudo mv replace/whitestone_master_schedule_staging_test.sqlx definitions/staging/
+sudo mv definitions/staging/whitestone_master_schedule_staging_prod.sqlx replace/
+sudo mv replace/whitestone_master_schedule_test.sqlx definitions/reporting/
+sudo mv definitions/reporting/whitestone_master_schedule_prod.sqlx replace/
+sudo mv replace/whitestone_private_equity_schedule_test.sqlx definitions/reporting/  
+sudo mv definitions/reporting/whitestone_private_equity_schedule_prod.sqlx replace/
 dataform run
