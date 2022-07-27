d29d2aaff7501d6722ba844603e30e748a3c2a10
Ryan den Otter <ryan.denotter@realist.ai>
2022-07-27 12:20:56 -0400

    Adding code to retreive the secrets correctly

--- access_secret.js
+++ access_secret.js
@@ -1,37 +1,28 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-function access_secret(company_name) {
-	const name = `projects/michael-gilbert-dev/secrets/company_name/versions/latest`;
-	// Instantiates a client
-	company_info = accessSecretVersion('michael-gilbert-dev', company_name, 'latest');
-	return company_info;
-}
+function accessSecret(project_name, company_name) {
 
-async function accessSecretVersion(projects, secret_id, versions) {
-	const client = new SecretManagerServiceClient();
-	const [version] = await client.accessSecretVersion({
-		name: company_name,
-	})
+    const name = `projects/${project_name}/secrets/${company_name}/versions/latest`;
 
-  // Extract the payload as a string.
-  const payload = version.payload.data.toString();
-  
-  // snippet is showing how to access the secret material.
-  console.info(`Payload: ${payload}`);
-  let company_info = {
-	  'username': payload['username'],
-	  'password': payload['password'],
-	  'dataset': payload['dataset'],
-	  'bucket_name': payload['bucket_name']
-	};
-  return company_info;
-//  const username = payload['username'] 
-//  const password = payload['password'] 
-//  const dataset = payload['dataset'] 
-//  const bucket_name = payload['bucket_name']
-//  return username, password, bucket_name, dataset;
+    // Import the Secret Manager Library
+     const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+    // Instantiate the client
+    const client = new SecretManagerServiceClient();
+
+    async function accessSecretVersion() {
+        const [version] = await client.accessSecretVersion({
+            name: name
+        });
+
+        // Extract the payload as a string
+        const payload = version.payload.data.toString();
+
+
+    }
+
+    accessSecretVersion();
 }
 module.exports = {
-	access_secret,
-	accessSecretVersion
+	accessSecret
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {execSync} = require("child_process");
 const { get_company_name } = require('./get_company_name.js');
-const { access_secret, accessSecretVersion } = require('./access_secret.js');
+const { accessSecret} = require('./access_secret.js');
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
  *
@@ -8,12 +8,15 @@ const { access_secret, accessSecretVersion } = require('./access_secret.js');
  * @param {!Object} context Metadata for the event.
  */
 exports.helloPubSub = (event, context) => {
+    // Hard coded value for Project Id for now. In the future, this should be
+    // defined as an evironment variable in the Cloud Function and read during
+    // runtime
+    const project_id = `michael-gilbert-dev`
+
 	const message = event.data
 		? Buffer.from(event.data, 'base64').toString()
     		: event.data;
 		company_name = get_company_name(message);
-		company_info = access_secret(company_name);
-		console.log(company_info);
+		company_info = accessSecret(project_id, company_name);
 //  execSync("./deploy_test.sh");
-
 };
