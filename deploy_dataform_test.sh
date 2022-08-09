2c2e8d029ef2f03cd010d616bea24d66042aec89
Michael Gilbert <michael.gilbert@realist.ai>
2022-08-09 14:46:01 +0000

    Changed the name of the deployed service

--- deploy_dataform_test.sh
+++ deploy_dataform_test.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy whitestone-dataform_test --service-account=whitestone-dataform@michael-gilbert-dev.iam.gserviceaccount.com --runtime=nodejs16 --entry-point=helloPubSub --trigger-topic=micahs_dataform_test
+gcloud functions deploy whitestone-dataform_invoice_test --service-account=whitestone-dataform@michael-gilbert-dev.iam.gserviceaccount.com --runtime=nodejs16 --entry-point=helloPubSub --trigger-topic=micahs_dataform_test
