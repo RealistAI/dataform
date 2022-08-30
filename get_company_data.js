343a4f96de8da88d76e18ec152f9eebf5dc1de34
Micah denOtter <micah.denotter@realist.ai>
2022-08-30 14:24:29 +0000

    almost working code, close to final stages

--- get_company_data.js
+++ get_company_data.js
@@ -3,32 +3,22 @@ const {BigQuery} = require('@google-cloud/bigquery');
 const {project_id_1} = require('./includes/constants.js');
 const bigquery = new BigQuery();
 
-let company_id = 'placeholder_6e49847f-11f7-462d-bb34-61ff460184ec';
-
 async function get_company_data(company_id) {
-  // Queries the U.S. given names dataset for the state of Texas
 
   const query = `SELECT company_id, phase, schedule_item
     FROM \`${project_id_1}.brain_test.schedule_info\` 
-    WHERE company_id = '${company_id}'`;
-  
-  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+    WHERE company_id = ${'company_id'}`;
   const options = {
     query: query,
-    // Location must match that of the dataset(s) referenced in the query.
-    location: 'US',
   };
 
   // Run the query as a job
   
-  const job = await bigquery.createQueryJob(options);
-  console.log(job);
+  const [job] = await bigquery.createQueryJob(options);
   // Wait for the query to finish
   
-  const [rows] = await job.getQueryResults();
-  console.log(rows);
+  const rows = await job.getQueryResults();
   return rows
 }
-get_company_data()
 
-//module.exports = {get_company_data};
+module.exports = { get_company_data };
--- phase_data.js
+++ phase_data.js
@@ -1,17 +1,17 @@
 const { get_company_data } = require('./get_company_data.js');
 
-let company_id = 'placeholder_6e49847f-11f7-462d-bb34-61ff460184ec';
+let company_id = 'placeholder_123443256';
 
 var phase_1 =[];
 
 
-function phases(get_company_data){ 
-  const data = get_company_data(company_id);
+(async ()=>{ 
+  const data = await get_company_data(company_id);
   for (var i in data) {
     if (data[i]['phase'] == 'phase_1')
-      phase_1.push(data[i]['schedule_item']);  
+      phase_1.push(data[i]['schedule_item']);
   };
-}
-console.log(phase_1);
+})()
 
+console.log(phase_1);
 
